d30996895936f56bf1e51524c3d28189b678bdde
tunnckoCore <mameto_100@mail.bg>
2015-04-19 09:52:33 +0300

    first commits

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * glob-fs <https://github.com/tunnckoCore/glob-fs>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function globFs() {
+  // body
+};
---
+++ test.js
@@ -0,0 +1,15 @@
+/**
+ * glob-fs <https://github.com/tunnckoCore/glob-fs>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var test = require('assertit');
+var globFs = require('./index');
+
+test('glob-fs:', function() {
+  // body
+});
